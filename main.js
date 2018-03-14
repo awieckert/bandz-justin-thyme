07b8460c484bd1354951dade909acb8d8a965e52
Jessica Rodgers <jessica.rodgers11@gmail.com>
2018-03-13 21:25:26 -0500

    added printToDom function

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 console.log('Justin-Thyme');
+
+function printToDom (stringToPrint, divId){
+    var thingToPrint = document.getElementById('divID');
+    thingToPrint.innerHTML += stringToPrint;
+}
+
+printToDom();
